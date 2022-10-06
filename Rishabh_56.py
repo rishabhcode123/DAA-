564f7abee6846f15d3a74f0afa032aa7849ecd22
rishabhcode123 <108607292+rishabhcode123@users.noreply.github.com>
2022-10-06 14:30:30 +0530

    Update Rishabh_56.py

--- Rishabh_56.py
+++ Rishabh_56.py
@@ -1,23 +1,23 @@
 import numpy as np;
 import random;
 
-num = int(input("Enter the number of towers: "))
+n = int(input("Enter the number of towers: "))
 tower = []
 
 choice = int(input("Enter 1 for random numbers and 2 for user input"))
 max_height = 10
 
 if choice == 1:
-    for i in range (0,num):
+    for i in range (0,n):
         temp = random.randrange(0, max_height, 1)
         tower.append(np.random.randint(20, size=temp))
 
 elif choice == 2:  
     print("Enter the matrix: ")
-    array = []
-    index = 0
+    arr = []
+    ind = 0
 
-    while index < num:
+    while ind < n:
         x = int(input(""))
         if x == -1:
             tower.append(np.array(arr))
@@ -26,7 +26,7 @@ elif choice == 2:
             ind += 1
             continue
         else:
-            array.append(x)
+            arr.append(x)
 else:
     print("Invalid Choice")
     quit()
@@ -41,7 +41,7 @@ for j in range (max_height, -1, -1):
             print(" ", end="\t")
     print()
 
-for i in range (0,num):
+for i in range (0,n):
     for x in range(len(tower[i]) - 1):
         if(tower[i][x] > tower[i][x + 1]):
             print("Not a Rising Tower")
